2484083efa98fa4679d88759814daba03fdc5766
unknown <mr.shahbaznns@gmail.com>
2026-03-29 19:13:17 +0500

    fix: restore full homepage content with balanced directives

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,38 @@
-Homepage works!
+<x-layout>
+    <x-slot name="title">Latest Jobs in Pakistan 2026 - Govt & Private Positions | JobsPic</x-slot>
+    
+    @push('breadcrumb_schema')
+    <script type="application/ld+json">
+    {
+      "@context": "https://schema.org",
+      "@type": "BreadcrumbList",
+      "itemListElement": [{
+        "@type": "ListItem",
+        "position": 1,
+        "name": "Home",
+        "item": "{{ url('/') }}"
+      }]
+    }
+    </script>
+    @endpush
+
+    <!-- Dynamic Block System -->
+    @if(isset($homeBlocks))
+        @foreach($homeBlocks as $block)
+            @php
+                $isFullWidth = in_array($block->type, ['newsletter', 'whatsapp_cta', 'hero_cards']);
+            @endphp
+            
+            @if($isFullWidth)
+                <div class="{{ $block->type === 'newsletter' ? '' : 'py-8' }}">
+                    @include('components.blocks.' . $block->type, ['block' => $block])
+                </div>
+            @else
+                <div class="mx-auto max-w-7xl px-4 lg:px-10 py-8">
+                    @include('components.blocks.' . $block->type, ['block' => $block])
+                </div>
+            @endif
+        @endforeach
+    @endif
+    <!-- End Dynamic Block System -->
+</x-layout>
