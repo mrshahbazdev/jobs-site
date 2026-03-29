a5000e6577d322c3c20672eda5a254870a235942
unknown <mr.shahbaznns@gmail.com>
2026-03-29 16:49:58 +0500

    perf: strip inter font to eliminate element render delay and rely on native tailwind system sans stack for instant LCP

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -18,12 +18,8 @@
         document.documentElement.classList.remove('dark');
         localStorage.theme = 'light';
     </script>
-    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    
-    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;900&display=swap" as="style">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;900&display=swap" rel="stylesheet">
     
     <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&display=swap" rel="stylesheet" media="print" onload="this.media='all'">
     
